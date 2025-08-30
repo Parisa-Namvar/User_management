bfa52bde752cb48f177773f240a84168e83d960b
pnamv <pnamvar22@gmail.com>
2025-08-30 13:08:19 -0700

    updated

---
+++ test/validation_module_test.py
@@ -0,0 +1,17 @@
+from re import match
+'''
+number validator
+
+print(match(r'^[\w\d]{8}$' ,'123456'))
+print(match(r'^[\w\d]{8}$' ,'12345678'))
+print(match(r'^[\w\d]{8}$' ,'kdhkfih'))
+print(match(r'^[\w\d]{8}$' ,'1254hhh'))
+print(match(r'^[\w\d]{8}$' ,'1234567896325'))
+
+user_name / nick_name validator
+print(match(r'^[a-zA-Z][\w.]{7,19}$', '123partsd'))
+print(match(r'^[a-zA-Z][\w.]{7,19}$', '.lfgrd4584'))
+print(match(r'^[a-zA-Z][\w.]{7,19}$', '-12gfgdg34'))
+print(match(r'^[a-zA-Z][\w.]{7,19}$', 'lkgurklkfxvstdrwfdefptgkrgiunjbjm'))
+print(match(r'^[a-zA-Z][\w.]{7,19}$', 'lmbkg1253'))
+'''
